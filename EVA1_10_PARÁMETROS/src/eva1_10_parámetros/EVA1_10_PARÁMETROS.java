1cdf30832b620ca8bdd26a2a9911209982228ed7
Yessly Mayorga <106281825+YessPuentes12@users.noreply.github.com>
2023-02-21 09:51:28 -0600

    Add files via upload

---
+++ EVA1_10_PARÁMETROS/src/eva1_10_parámetros/EVA1_10_PARÁMETROS.java
@@ -0,0 +1,23 @@
+/*
+
+ */
+package eva1_10_parámetros;
+
+
+
+
+public class EVA1_10_PARÁMETROS {
+
+ 
+  public static void main(String[] args) {
+    nPrintln("HOLA", 5); 
+    }
+  
+   public static void nPrintln (String message, int n){
+   for (int i=0; i<n; i++){
+    System.out.println(message);    
+    }  
+  }  
+   
+   
+}
